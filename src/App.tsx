d56e6bc3661384085eb58bc5519181e6e8d37206
AlexandreLeonardo12 <alexandreleonardo3746@gmail.com>
2025-07-28 01:12:14 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
 
 const queryClient = new QueryClient();
 
@@ -15,9 +17,11 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+              <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
+                  <Route path="/projects" element={<Projects />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
