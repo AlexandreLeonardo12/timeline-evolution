b833fb68aad35ded28a72675b20176cf9a749ef8
AlexandreLeonardo12 <alexandreleonardo3746@gmail.com>
2025-08-03 15:39:05 -0700

    Use HashRouter for SPA routing to fix 404 on project detail

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -23,7 +23,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           {/* Ensure the page scrolls to top on route change */}
           <ScrollToTop />
           <Navbar />
@@ -37,7 +37,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </LanguageProvider>
